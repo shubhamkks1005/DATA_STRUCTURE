c32ac8c38d83b1931dab9ad31712d2e4d2609d58
Shubham Kumar kushwaha <shubhamkks1005@gmail.com>
2025-07-01 11:11:27 +0530

    written binary code

--- GRAPH/graph.cpp
+++ GRAPH/graph.cpp
@@ -1,4 +1,21 @@
-#include
-gvdhucqd
-idw
-h h  g j jvjjjj  
+#include<iostream>
+using namespace std;
+int main()
+{
+  int arr[1000];
+  int size,key;
+  cout<<"size of array =";
+  cin>>size;
+  cout<<"\nenter "<<size<<"elements(elements must be sorted)\n";
+  //to take input(array)
+  for (int i = 0; i < size; i++)
+  {
+    cin>>arr[i];
+    cout<<"  ";
+  }
+
+  cout<<"enter element you want to search";
+  cin>>key;
+  
+
+}
