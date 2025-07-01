b2e0ebc517cc2ab29aaa60e1c70674592420e97a
Shubham Kumar kushwaha <shubhamkks1005@gmail.com>
2025-07-01 12:22:08 +0530

    problem1

---
+++ BINARY SEARCH/problem_1.cpp
@@ -0,0 +1,24 @@
+//Problem:-First and last position a element is sorted array.
+#include<iostream>
+using namespace std;
+void main()
+{
+   int arr[1000];
+  int size,key;
+  cout<<"size of array =";
+  cin>>size;
+  cout<<"\nenter "<<size<<"elements(elements must be sorted)\n";
+  //to take input(array)
+  for (int i = 0; i < size; i++)
+  {
+    cin>>arr[i];
+    
+  }
+
+  cout<<"enter element, whom you want to search first and last position";
+  cin>>key;
+
+  
+
+
+}
