978c48fbe2fa85ccdb1b8b44d5feaa28a576d207
Shubham Kumar kushwaha <shubhamkks1005@gmail.com>
2025-07-01 11:52:14 +0530

    bianry code

--- BINARY SEARCH/binary_search.cpp
+++ BINARY SEARCH/binary_search.cpp
@@ -1,3 +1,5 @@
+//problem:- search any element using binary search in array
+// take array from user
 #include<iostream>
 using namespace std;
 int main()
@@ -11,11 +13,39 @@ int main()
   for (int i = 0; i < size; i++)
   {
     cin>>arr[i];
-    cout<<"  ";
+    
   }
 
   cout<<"enter element you want to search";
   cin>>key;
+  int first=0,last=size-1,mid,count=0;
+  mid=first+(last-first)/2;
+  while (first<=last)
+  {
+      if (key==arr[mid])
+      {
+          cout<<"element found";
+          count=1;
+          break;
+        }
+        if (key>arr[mid])
+        {
+            first=mid+1;
+        }
+        if (key<arr[mid])
+        {
+            last=mid-1;
+        }
+        mid=first+(last-first)/2;
+            
+            
+     }
+        
+ if (count==0)
+ {
+   cout<<"element not found";
+ }
+ 
   
 
 }
