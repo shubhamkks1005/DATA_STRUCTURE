c3a5d43f1d61ea93ab503725a712dd0a650287f2
Shubham Kumar kushwaha <shubhamkks1005@gmail.com>
2025-07-01 13:30:38 +0530

    problem 1 solved

--- BINARY SEARCH/problem_1.cpp
+++ BINARY SEARCH/problem_1.cpp
@@ -1,7 +1,57 @@
 //Problem:-First and last position a element is sorted array.
 #include<iostream>
 using namespace std;
-void main()
+ int star( int *a,int key, int last) //gives first occurence
+ {  int first=0;
+    int mid=first+(last-first)/2;
+    int count=-1;
+   
+   
+   
+    while (first<=last)
+    {
+        if (key==a[mid])
+        {
+            last=mid-1;
+            count=1;
+        }else if (key<a[mid])
+        {
+          last=mid-1;
+        }else if (key>a[mid])
+        {
+           first=mid+1;
+        }
+        
+        
+        mid=first+(last-first)/2;
+    }
+    
+    if (count==-1)
+    {
+       return -1;
+    }
+    else return mid;
+ }
+ int las( int *a,int key,int first,int last)
+{
+ 
+    int mid=first+(last-first)/2;
+    
+   while (first<=last)
+    {
+        if (key==a[mid])
+        {
+            first=mid+1;
+            
+        }else if (key<a[mid])
+            last=mid-1;
+           mid=first+(last-first)/2;
+
+    }
+    return mid;
+
+}
+ int main()
 {
    int arr[1000];
   int size,key;
@@ -17,8 +67,19 @@ void main()
 
   cout<<"enter element, whom you want to search first and last position";
   cin>>key;
+  int first=0,last=size-1;
+  int start,end;
+  start=star(arr,key,last);
+  if (start==-1)
+  {
+    cout<<"element not found";
+    return 1;
+ }
+ cout<<"\n start="<<start;
 
-  
+   end=las(arr,key,start,last);
+   cout<<"end="<<end;
 
 
+     
 }
