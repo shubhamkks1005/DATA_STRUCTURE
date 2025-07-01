7d80f4fa70576c6a09002136c69b9ad4e07b438c
Shubham Kumar kushwaha <shubhamkks1005@gmail.com>
2025-07-01 23:14:33 +0530

    .

--- BINARY SEARCH/problem_3.cpp
+++ BINARY SEARCH/problem_3.cpp
@@ -3,24 +3,37 @@
 using namespace std;
 int main()
 {
-  int num;
+ 
+    int num;
   cout<<"enter number whose you want to find square root"<<endl;
   cin>>num;
   int first=1,last=num,sqr;
+  if (num<2)  //to cover 0 and 1
+  {
+   sqr=num;
+   return 0;
+  }
+  
   int mid=first+(last-first)/2;
   while (first<=last)
-  {
-    if (mid*mid>num)
+  { 
+    if (mid>num/mid) // convet mid*mid>num to mid>num/mid to overcome overflow
     {
         last=mid-1;
     }
-    if (mid*mid<=num)
+    if (mid<num/mid)
     {
+        sqr=mid;
        first=mid+1;
-       sqr=mid;
     }
-     mid=first+(last-first)/2;
+    if (mid==num/mid)
+    {
+        sqr=mid;
+        break;
+    }
+    
+    mid=first+(last-first)/2;
   }
   
-cout<<"square ="<<mid;
+cout<<"square ="<<sqr;
 }
