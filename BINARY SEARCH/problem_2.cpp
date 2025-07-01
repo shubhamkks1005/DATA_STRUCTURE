00c24c0ad8df6c2a25e8f2368a666fb311917d66
Shubham Kumar kushwaha <shubhamkks1005@gmail.com>
2025-07-01 17:05:55 +0530

    .

---
+++ BINARY SEARCH/problem_2.cpp
@@ -0,0 +1,41 @@
+//problem:-what will be the position of element if it is present in sorted array.
+#include<iostream>
+using namespace std;
+int main()
+{int arr[1000];
+  int size,key;
+  cout<<"size of array =";
+  cin>>size;
+  cout<<"\nenter "<<size<<"elements(elements must be sorted)\n";
+  //to take input(array)
+  for (int i = 0; i < size; i++)
+  {
+    cin>>arr[i];
+    
+  }
+
+  cout<<"enter element";
+  cin>>key;
+  int first=0,last=size-1;
+  int mid=first+(last-first)/2;
+  while (first<=last)
+  {
+    if (arr[mid]==key)
+    {
+       
+       break;
+    }
+    else if (key<arr[mid])
+    {
+        last=mid-1;
+    }
+    else if (key>arr[mid])
+    {
+        first=mid+1;
+    }
+     mid=first+(last-first)/2;
+  }
+  cout<<mid;
+  return 0;
+
+}
