ae58714d276574031da3ed1743897b41d9b2591f
Shubham Kumar kushwaha <shubhamkks1005@gmail.com>
2025-07-01 11:13:44 +0530

    Update graph.cpp

--- GRAPH/graph.cpp
+++ GRAPH/graph.cpp
@@ -2,20 +2,7 @@
 using namespace std;
 int main()
 {
-  int arr[1000];
-  int size,key;
-  cout<<"size of array =";
-  cin>>size;
-  cout<<"\nenter "<<size<<"elements(elements must be sorted)\n";
-  //to take input(array)
-  for (int i = 0; i < size; i++)
-  {
-    cin>>arr[i];
-    cout<<"  ";
-  }
-
-  cout<<"enter element you want to search";
-  cin>>key;
+  
   
 
 }
