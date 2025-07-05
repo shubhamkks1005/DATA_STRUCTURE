3fcccf6c552a2f85afa769237a350f885fa7a348
Shubham Kumar kushwaha <shubhamkks1005@gmail.com>
2025-07-05 21:37:59 +0530

    search nth missing number

---
+++ BINARY SEARCH/problem_7.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int arr[1000];
+    int size,key;
+    cout<<"enter size of array=";
+    cin>>size;
+    for (int i = 0; i < size; i++)
+    {
+       cin>>arr[i];
+    }
+    cout<<"search nth missing number";
+    cin>>key;
+    int first=0,last=size-1,mid,result;
+    while (first<=last)
+    {  mid=first+(last-first)/2;
+        if (arr[mid]-mid-1>=key)
+        {
+            result=mid;
+            last=mid-1;
+        }
+        else first=mid+1;
+     }
+
+     cout<<result+key;
+    
+}
